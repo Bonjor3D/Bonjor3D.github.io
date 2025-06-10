79020044d0775c48e39adc3e541b15cf5bf06c13
Bonjor3D <bonior3d@gmail.com>
2025-06-10 20:11:13 +0300

    1.0.1 Изменения

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Пока пустой, можно добавить анимации или интерактив
+const button = document.querySelector('#buttonMain');
+const info = document.querySelector('#info');
+const container = document.querySelector('.container');
+const avatar = document.querySelector('#avatarBg');
+const content = document.querySelector('#content');
+
+button.addEventListener('click', () => {
+  container.classList.toggle('active');
+  button.classList.toggle('active');
+  avatar.classList.toggle('active');
+  content.classList.toggle('active');
+});
